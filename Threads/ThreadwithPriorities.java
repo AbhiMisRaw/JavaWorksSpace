90ca7a1a941f891edbd0859e2a704ef13844d8fb
AbhiMisRaw <88389429+AbhiMisRaw@users.noreply.github.com>
2022-06-24 14:21:02 +0530

    Add files via upload

---
+++ Threads/ThreadwithPriorities.java
@@ -0,0 +1,11 @@
+package UnitFour;
+
+
+public class ThreadwithPriorities extends Thread{
+    @Override
+    public void run() {
+        super.run();
+        System.out.println("Thread with Priorities");
+    }
+}
+
